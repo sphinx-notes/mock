59a2c57c0b590c0b0ceaf2d79693fbe0e73ecf2d
Shengyu Zhang <i@silverrainz.me>
2022-08-14 16:00:18 +0800

    Add confval mock_default_mode; support literal mock mode

--- sphinxnotes/mock/__init__.py
+++ sphinxnotes/mock/__init__.py
@@ -1,12 +1,4 @@
-"""
-    sphinxnotes.mock
-    ~~~~~~~~~~~~~~~
-
-    Sphinx extension for mocking directives and roles.
-
-    :copyright: Copyright 2022 Shengyu Zhang
-    :license: BSD, see LICENSE for details.
-"""
+""" Sphinx extension for mocking directives and roles. """
 
 from __future__ import annotations
 from typing import List, Dict
@@ -17,19 +9,20 @@ from sphinx.config import Config
 from sphinx.util.docutils import SphinxDirective
 
 from docutils.parsers.rst import directives
+from docutils import nodes
 
 __title__= 'sphinxnotes-mock'
 __license__ = 'BSD'
-__version__ = '1.0.0b0'
+__version__ = '1.0.0'
 __author__ = 'Shengyu Zhang'
 __url__ = 'https://sphinx-notes.github.io/mock'
-__description__ = ' Sphinx extension for mocking directives and roles'
+__description__ = 'Sphinx extension for mocking directives and roles'
 __keywords__ = 'documentation, sphinx, extension'
 
 logger = logging.getLogger(__name__)
 
 class MockOptionSpec(Dict):
-    def __getitem__(self, key):
+    def __getitem__(self, _):
         return directives.unchanged
 
 
@@ -40,18 +33,36 @@ class MockDirective(SphinxDirective):
     has_content = True
 
     def run(self) -> List[nodes.Node]:
-        return []
+        mode = None
+        for d in self.config.mock_directives:
+            name = d if isinstance(d, str) else d[0]
+            if self.name != name:
+                continue
+            mode = self.config.mock_mode if isinstance(d, str) else d[1]
+            break
+
+        if mode == 'literal':
+            literal = nodes.literal_block(self.block_text, self.block_text)
+            literal['language'] = 'rst'
+            return [literal]
+        elif mode == 'hide':
+            return []
+        else:
+            raise ValueError('unsupported mock mode')
+
 
 
 def _config_inited(app:Sphinx, config:Config) -> None:
-    for d in app.config.mock_directives:
-        app.add_directive(d, MockDirective, override=True)
+    for d in config.mock_directives:
+        name = d if isinstance(d, str) else d[0]
+        app.add_directive(name, MockDirective, override=True)
 
 
-def setup(app:Sphinx) -> None:
+def setup(app:Sphinx) -> Dict:
     """Sphinx extension entrypoint."""
 
-    app.add_config_value('mock_directives', [], 'env', types=List[str])
+    app.add_config_value('mock_directives', [], 'env')
+    app.add_config_value('mock_mode', 'hide', 'env')
     app.connect('config-inited', _config_inited)
 
     return {'version': __version__}
